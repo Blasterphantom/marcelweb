5e24fc72a7e2a3a0140d347f00e5c0646082d72c
Marcel Rodriguez <marcelrodriguezcoding@gmail.com>
2024-04-17 20:52:38 -0700

    Rm useReducer

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect, useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../About/About.css';
 
